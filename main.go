bad5ff6512cf1eafb3d130f6408812ca713e91df
Indraneel Dutta <indraneel.dutta@hotmail.com>
2020-05-05 03:32:57 +0530

    added getComments API

--- main.go
+++ main.go
@@ -3,14 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
-	"math"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -22,9 +19,9 @@ var (
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/movies", GetMovies).Methods("GET")
-	router.HandleFunc("/movies/{name}", GetMovies).Methods("GET")
-	router.HandleFunc("/addrating", AddRating).Methods("POST")
+	router.HandleFunc("/movies", handleMovies).Methods("GET")
+	router.HandleFunc("/movies/{name}", handleMovies).Methods("GET")
+	router.HandleFunc("/addrating", handleRating).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
@@ -42,78 +39,18 @@ func GetClient() *mongo.Client {
 	return client
 }
 
-type Movie struct {
-	Name    string  `json:"name"`
-	Rating  float64 `json:"rating"`
-	RatedBy float64 `json:"ratedBy"`
-}
-
-type User struct {
-	UserName string     `json:"userName"`
-	Rated    []Rated    `json:"rated,omitempty"`
-	Comments []Comments `json:"comments,omitempty"`
-}
-
-type Rated struct {
-	Movie  string  `json:"movie"`
-	Rating float64 `json:"rating"`
-}
-
-type Comments struct {
-	Movie   string `json:"movie"`
-	Comment string `json:"comments"`
-}
-
-type RatingRequest struct {
-	UserName string  `json:"userName"`
-	Movie    string  `json:"movie"`
-	Rating   float64 `json:"rating"`
-}
-
-// GetMovies returns for the movies searched or all movies
-func GetMovies(w http.ResponseWriter, r *http.Request) {
-	var movies []*Movie
-
-	collection := client.Database("Movies").Collection("movies")
-	query := primitive.M{
-		"name": primitive.Regex{
-			Pattern: mux.Vars(r)["name"],
-			Options: "i",
-		},
-	}
-	cur, err := collection.Find(context.TODO(), query)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode("")
-		return
-	}
-
-	for cur.Next(context.TODO()) {
-		var movie Movie
-		err = cur.Decode(&movie)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode("")
-		}
-		movies = append(movies, &movie)
-	}
-
+func handleMovies(w http.ResponseWriter, r *http.Request) {
+	response := GetMovies(mux.Vars(r)["name"])
 	w.Header().Set("Content-Type", "application/json")
-
-	if len(movies) == 0 {
-		w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusOK)
+	if response.Body == nil {
 		json.NewEncoder(w).Encode("No movies found")
 	} else {
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(movies)
+		json.NewEncoder(w).Encode(response)
 	}
 }
 
-func AddRating(w http.ResponseWriter, r *http.Request) {
-	var user User
-
-	collection := client.Database("Movies").Collection("users")
-
+func handleRating(w http.ResponseWriter, r *http.Request) {
 	var reqBody RatingRequest
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -128,69 +65,8 @@ func AddRating(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	query := primitive.M{
-		"username": reqBody.UserName,
-	}
-	cur := collection.FindOne(context.TODO(), query)
-	err = cur.Decode(&user)
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode("User not Found")
-		return
-	}
-
-	rating := Rated{
-		Movie:  reqBody.Movie,
-		Rating: reqBody.Rating,
-	}
-	user.Rated = append(user.Rated, rating)
-
-	_, err = collection.UpdateOne(
-		context.TODO(),
-		primitive.M{
-			"username": reqBody.UserName,
-		},
-		primitive.D{
-			{"$set", primitive.D{{"rated", user.Rated}}},
-		},
-	)
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode("")
-	}
-
-	collection = client.Database("Movies").Collection("movies")
-	var movie Movie
-	query = primitive.M{
-		"name": reqBody.Movie,
-	}
-	cur = collection.FindOne(context.TODO(), query)
-	err = cur.Decode(&movie)
-
-	newRating := ((movie.Rating*movie.RatedBy)+reqBody.Rating)/movie.RatedBy + 1
-
-	newRating = math.Round(newRating*100) / 100
-
-	fmt.Println(newRating)
-	fmt.Println(movie)
-
-	_, err = collection.UpdateOne(
-		context.TODO(),
-		primitive.M{
-			"name": reqBody.Movie,
-		},
-		primitive.D{
-			{"$set", primitive.D{{"rating", newRating}}},
-			{"$set", primitive.D{{"ratedBy", movie.RatedBy + 1}}},
-		},
-	)
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode("")
-	} else {
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode("Rating updated successfully")
-	}
+	response := AddRating(reqBody)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(response.Status)
+	json.NewEncoder(w).Encode(response)
 }
---
+++ movies.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type ResponseMovies struct {
+	Status int      `json:"status"`
+	Body   []*Movie `json:"body"`
+}
+
+type Movie struct {
+	Name     string    `json:"name"`
+	Rating   float64   `json:"rating"`
+	RatedBy  float64   `json:"ratedBy"`
+	Comments []Comment `json:"comments"`
+}
+
+type User struct {
+	UserName string  `json:"userName"`
+	Rated    []Rated `json:"rated,omitempty"`
+}
+
+type Rated struct {
+	Movie  string  `json:"movie"`
+	Rating float64 `json:"rating"`
+}
+
+type Comment struct {
+	UserName string `json:"username"`
+	Comment  string `json:"comment"`
+}
+
+type RatingRequest struct {
+	UserName string  `json:"userName"`
+	Movie    string  `json:"movie"`
+	Rating   float64 `json:"rating"`
+}
+
+type ResponseRating struct {
+	Status int    `json:"status"`
+	Body   string `json:"body"`
+}
+
+type ResponseComment struct {
+	Movie    string    `json:"movie"`
+	Comments []Comment `json:"comments"`
+}
+
+// GetMovies returns for the movies searched or all movies
+func GetMovies(movieName string) ResponseMovies {
+	var movies []*Movie
+	var response ResponseMovies
+
+	collection := client.Database("Movies").Collection("movies")
+	query := primitive.M{
+		"name": primitive.Regex{
+			Pattern: movieName,
+			Options: "i",
+		},
+	}
+	cur, err := collection.Find(context.TODO(), query)
+	if err != nil {
+		return response
+	}
+
+	for cur.Next(context.TODO()) {
+		var movie Movie
+		err = cur.Decode(&movie)
+		if err != nil {
+			return response
+		}
+		responseComment := GetComments(movie.Name)
+		movie.Comments = responseComment.Comments
+		movies = append(movies, &movie)
+	}
+
+	response = ResponseMovies{
+		Status: http.StatusOK,
+		Body:   movies,
+	}
+
+	return response
+}
+
+func AddRating(req RatingRequest) ResponseRating {
+	var user User
+
+	collection := client.Database("Movies").Collection("users")
+
+	query := primitive.M{
+		"username": req.UserName,
+	}
+	cur := collection.FindOne(context.TODO(), query)
+	err := cur.Decode(&user)
+	for _, value := range user.Rated {
+		if value.Movie == req.Movie {
+			return ResponseRating{
+				Status: http.StatusOK,
+				Body:   "Movie is already rated by user",
+			}
+		}
+	}
+	if err != nil {
+		return ResponseRating{
+			Status: http.StatusNotFound,
+			Body:   "User not found",
+		}
+	}
+
+	rating := Rated{
+		Movie:  req.Movie,
+		Rating: req.Rating,
+	}
+	user.Rated = append(user.Rated, rating)
+
+	_, err = collection.UpdateOne(
+		context.TODO(),
+		primitive.M{
+			"username": req.UserName,
+		},
+		primitive.D{
+			{"$set", primitive.D{{"rated", user.Rated}}},
+		},
+	)
+
+	if err != nil {
+		return ResponseRating{
+			Status: http.StatusNotFound,
+			Body:   "User not found",
+		}
+	}
+
+	getMovie := GetMovies(req.Movie)
+	movieDetails := getMovie.Body[0]
+
+	newRating := ((movieDetails.Rating*movieDetails.RatedBy)+req.Rating)/movieDetails.RatedBy + 1
+
+	newRating = math.Round(newRating*100) / 100
+
+	_, err = collection.UpdateOne(
+		context.TODO(),
+		primitive.M{
+			"name": req.Movie,
+		},
+		primitive.D{
+			{"$set", primitive.D{{"rating", newRating}}},
+			{"$set", primitive.D{{"ratedBy", movieDetails.RatedBy + 1}}},
+		},
+	)
+
+	if err != nil {
+		return ResponseRating{
+			Status: http.StatusInternalServerError,
+			Body:   "Failed to get movie",
+		}
+	}
+
+	return ResponseRating{
+		Status: http.StatusOK,
+		Body:   "Rating updated successfully",
+	}
+}
+
+func GetComments(movieName string) ResponseComment {
+	var responseComment ResponseComment
+	collection := client.Database("Movies").Collection("comments")
+	query := primitive.M{
+		"movie": movieName,
+	}
+	cur := collection.FindOne(context.TODO(), query)
+	err := cur.Decode(&responseComment)
+
+	if err != nil {
+		return responseComment
+	}
+
+	fmt.Println(responseComment)
+	return responseComment
+}
